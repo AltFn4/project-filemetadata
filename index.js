150f2a132e8b28f49c87718b1c4928adf545a637
AltFn4 T <tris31415926@gmail.com>
2025-02-21 09:26:50 +0000

    Added file upload endpoint.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  var file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  });
+});
 
 
 const port = process.env.PORT || 3000;
